95e39012650e27b95dd1ce7bb29c65d3023e5c93
Laggstar <ksjolly5@gmail.com>
2022-05-21 01:53:02 -0400

    Debug frontend

--- frontend/app/commands/get_challenge.py
+++
@@ -1,9 +0,0 @@
-'''
-Author: Laggstar
-'''
-
-import requests
-
-async def get_chall_from_db(ctx, url):
-    #{ "id" : 1, "author: : "author", "title" : "title", "challenge" : "Some challenge description"}
-    return requests.get(url).content()
