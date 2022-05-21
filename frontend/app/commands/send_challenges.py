a4ba4b21e3aea9b0f0801f163a7d1f911b844ba8
Laggstar <ksjolly5@gmail.com>
2022-05-21 01:53:02 -0400

    Debug frontend

--- frontend/app/commands/send_challenges.py
+++ frontend/app/commands/send_challenges.py
@@ -2,19 +2,21 @@
 Author: Laggstar
 '''
 
-import discord
+import discord, requests
 from discord.ext import commands
-from get_challenge import *
 
 async def send_chall(ctx, message):
     # print(message)
     if str(message) == "challenge":
+        url = "http://backend:8080"
         #{ "id" : 1, "author: : "author", "title" : "title", "challenge" : "Some challenge description"}
-        challdict = await get_chall_from_db()
+        x = requests.get(url)
+        challdict = x.json()[-1]
+        print(challdict)
         # Get Challenge Name
-        embed = discord.Embed(title = challdict["title"], color=discord.Color.blue())
+        embed = discord.Embed(title = challdict["Title"], color=discord.Color.blue())
         # Get Challenge author and description
-        embed.add_field(name = challdict["author"], value = challdict["challenge"], inline = False)
+        embed.add_field(name = challdict["Author"], value = challdict["Description"], inline = False)
         # Footer
         embed.set_footer(text = "Send you answer answer as a file")
         await ctx.message.author.send(embed = embed)
