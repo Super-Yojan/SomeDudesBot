987802cbc4e67e4e53e949cc7d8df91d0e53b7eb
Laggstar <ksjolly5@gmail.com>
2022-05-20 23:51:08 -0400

    send and get challenge edited (not done)

--- frontend/app/commands/remove_roles.py
+++ frontend/app/commands/remove_roles.py
@@ -3,13 +3,17 @@ Author: Laggstar
 '''
 
 async def remove_all_roles(ctx, bot):
+    # Server list 
     serverlist = ["Programmer's Paradise"]
     for guild in bot.guilds:
         if str(guild) in serverlist:
             for member in guild.members:
+                # Checks if the user is a bot
                 if (member.bot == False):
                     for role in member.roles:
                         try:
+                            # Remove all roles
                             await member.remove_roles(role, reason = "Clearing roles", atomic = True)
+                        # @everyone/other roles which can't be removed
                         except:
                             print("Role:", role, "not removed for", str(member) + ".")
