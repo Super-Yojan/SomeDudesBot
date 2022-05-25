5e744167e8a7e9940e584d3f5fe396173b9eb08e
Laggstar <ksjolly5@gmail.com>
2022-05-25 01:38:14 -0400

    Adding

--- frontend/main.py
+++ frontend/main.py
@@ -8,7 +8,7 @@ intents = discord.Intents.default()
 intents.members = True
 bot = commands.Bot(command_prefix='/', intents = intents)
 
-from app.commands import remove_roles, add_roles, send_challenges, solve_challenges
+from app.commands import remove_roles, add_user_roles, send_challenges
 
 '''
  All the discord bot commands will be imported here and 
@@ -19,11 +19,11 @@ from app.commands import remove_roles, add_roles, send_challenges, solve_challen
 async def remove_user_roles(ctx):
     await remove_roles.remove_all_roles(ctx, bot)
 
-
+@commands.has_permissions(administrator=True) 
 @commands.has_permissions(manage_roles=True)
 @bot.command()
 async def add_roles(ctx):
-    await add_roles.add_role(ctx,"Kabir",["Laggstar#4897"])
+    await add_user_roles.add_role(ctx,"Kabir",["Laggstar"])
 
 @bot.command()
 async def send_challenge(ctx, message):
@@ -31,11 +31,6 @@ async def send_challenge(ctx, message):
         await send_challenges.send_chall(ctx, message)
     else:
         await ctx.channel.send("A bot can't ask for a challenge!")
-
-
-@bot.listen()
-async def on_message(message):
-    await solve_challenges.solve_chall(message)
     
 
 token = os.environ['DISCORD_TOKEN']
