eae1388ef1ad09b6f93a0118c04aaf603d85a898
Yojan <gautamyojan0@gmail.com>
2022-05-25 01:44:35 -0400

    complete

--- frontend/app/commands/add_user_roles.py
+++ frontend/app/commands/add_user_roles.py
@@ -1,27 +1,22 @@
 '''
 Author: Laggstar
 '''
-from discord.ext import commands
+
+import discord
 
 async def add_role(ctx, role, names):
     # Search for role in server
     for name in names:
-        # print("String of role: " + str(role) + ", roles list: " , ctx.guild.roles)
         if str(role) not in str(ctx.guild.roles):
             await create_Role(ctx, str(role), names)
         else:
-            await assign_Role(ctx, role, names)
+            await assign_Role(ctx.author, role)
 
 async def create_Role(ctx, role, names):
     await ctx.guild.create_role(name = role)
     await ctx.send(f'Role `{role}` has been created')
-    await assign_Role(ctx, role, names)
+    await assign_Role(ctx.author,role)
 
-@commands.has_permissions(administrator=True) 
-async def assign_Role(ctx, role, names):
-    for name in names:
-        for member in ctx.guild.members:
-            if name == str(member) and member.bot == False:
-                if role not in member.roles:
-                    print("Member: " + member)
-                    await member.add_roles(role)
+async def assign_Role(user,role):
+    await user.add_roles(discord.utils.get(user.guild.roles, name=role))
+
