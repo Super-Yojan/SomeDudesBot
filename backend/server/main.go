236dc9a4673d9b2f27c544c089445fc3b7da3a72
Yojan <gautamyojan0@gmail.com>
2022-06-12 16:42:50 -0400

    updated

--- backend/server/main.go
+++ backend/server/main.go
@@ -1,18 +1,16 @@
 package main
 
 import (
-    "somedude/helpers"
-	_ "github.com/lib/pq"
 	"github.com/gofiber/fiber/v2"
-    "somedude/data_access"
+	_ "github.com/lib/pq"
+	"somedude/data_access"
+	"somedude/helpers"
 )
 
-
-
 // The starting place for the app..
-func main(){
-    data_access.Migrate() 
-    app := fiber.New()
-    helpers.SetupRoutes(app)
-    app.Listen(":8080")
+func main() {
+	data_access.Migrate()
+	app := fiber.New()
+	helpers.SetupRoutes(app)
+	app.Listen(":8080")
 }
