e3c6a6b154b85043496cc70fb4a16e3038689633
Yojan <gautamyojan0@gmail.com>
2022-06-26 13:43:09 -0400

    fix

--- frontend/main.py
+++ frontend/main.py
@@ -6,7 +6,7 @@ import os
 
 intents = discord.Intents.default()
 intents.members = True
-bot = commands.Bot(intents = intents)
+bot = commands.Bot(command_prefix='/', intents = intents)
 
 from app.commands import remove_roles, add_user_roles, send_challenges,solve_challenges
 
@@ -15,7 +15,6 @@ from app.commands import remove_roles, add_user_roles, send_challenges,solve_cha
  configured here.
 '''
 
-# @bot.command()
 @bot.slash_command(guild_ids=[930653648705454120])
 async def remove_roles(ctx):
     await remove_roles.remove_all_roles(ctx, bot)
@@ -26,7 +25,6 @@ async def on_message(message):
     member = server.get_member(message.author.id)
     await solve_challenges.solve_chall(message,member)
 
-# @bot.command()
 @bot.slash_command(guild_ids=[930653648705454120])
 async def send_chall(ctx, message):
     if ctx.author.bot == False:
