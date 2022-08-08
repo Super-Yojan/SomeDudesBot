035527edabb7dccac0ee3ced77ec4f82eeb8cb95
Yojan <gautamyojan0@gmail.com>
2022-08-07 21:37:27 -0400

    dunno

--- backend/server/dockerfiles/PalindromeNumber/python/PalindromeNumberTest.py
+++ backend/server/dockerfiles/PalindromeNumber/python/PalindromeNumberTest.py
@@ -5,18 +5,11 @@ import random
 class TestPalindrom(unittest.TestCase):
 
     def solution(self, x):
-        new_num = 0
-        copy_num = x
-        
-        while(copy_num/10>0):
-            new_num = (new_num*10) + copy_num %10
-            copy_num = copy_num // 10
-        
-        return new_num == x
+        return str(x)[::-1] == str(x)
 
 
     def test_sum(self):
-        for i in range(300000):
+        for i in range(30000):
             num = random.randint(0,i)
             user = palindrome_number(num)
             actual = self.solution(num)
