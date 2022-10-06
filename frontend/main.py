bc9aff56a0e9c20e093a92ff97d1b82b9084e45b
Yojan Gautam <gautamyojan0@gmail.com>
2022-10-06 15:55:39 -0400

    update remove roles

--- frontend/main.py
+++ frontend/main.py
@@ -1,3 +1,4 @@
+from app.commands import remove_roles, add_user_roles, send_challenges, solve_challenges
 import discord
 from discord.ext import commands
 import asyncio
@@ -6,24 +7,26 @@ import os
 
 intents = discord.Intents.default()
 intents.members = True
-bot = commands.Bot(command_prefix='/', intents = intents)
+bot = commands.Bot(command_prefix='/', intents=intents)
 
-from app.commands import remove_roles, add_user_roles, send_challenges,solve_challenges
 
 '''
  All the discord bot commands will be imported here and 
  configured here.
 '''
 
+
 @bot.slash_command(guild_ids=[930653648705454120])
-async def remove_roles(ctx):
+async def clear_roles(ctx):
     await remove_roles.remove_all_roles(ctx, bot)
+
 
 @bot.listen()
 async def on_message(message):
     server = bot.get_guild(930653648705454120)
     member = server.get_member(message.author.id)
-    await solve_challenges.solve_chall(message,member)
+    await solve_challenges.solve_chall(message, member)
+
 
 @bot.slash_command(guild_ids=[930653648705454120])
 async def send_chall(ctx):
