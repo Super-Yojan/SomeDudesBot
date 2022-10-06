f4d06c50e64ea88c028ac020beee2b31e96a52c6
Yojan Gautam <gautamyojan0@gmail.com>
2022-10-06 15:21:05 -0400

    changed go server

--- backend/server/model/challenge.go
+++ backend/server/model/challenge.go
@@ -1,15 +1,15 @@
 package model
 
-type Challenge struct{
-    ID          int64   
-    Author      string  
-    Title       string 
-    Description string
+type Challenge struct {
+	ID          int64  `json:"ID"`
+	Author      string `json:"Author"`
+	Title       string `json:"Title"`
+	Description string `json:"Description"`
 }
 
-type Solve struct{
-    ID          int64   `json:"ID"`
-    User        string  `json:"User"`
-    Title       string  `json:"Title"`
-    File        string  `json:"File"`
+type Solve struct {
+	ID    int64  `json:"ID"`
+	User  string `json:"User"`
+	Title string `json:"Title"`
+	File  string `json:"File"`
 }
